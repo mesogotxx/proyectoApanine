705e8f3136ee18ff7bbd9781f842dbc6c3674bc5
Santiajo <ca.manzo@duocuc.cl>
2024-11-12 23:20:23 -0300

    Se borró la carpeta posts, la cual tenía una views en desuso, y se quitó del controlador la ruta que redirigía a este archivo en desuso

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        return view('posts.index');
-    }
     public function login()
     {
         return view('login.login');
