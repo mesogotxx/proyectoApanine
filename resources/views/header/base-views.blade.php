0b2de0891450de0f0da16ccbdc07f366a7c3280a
Santiajo <ca.manzo@duocuc.cl>
2024-11-14 18:51:55 -0300

    modified:   public/css/sidebar.css modified:   resources/views/header/base-views.blade.php Nueva letra

--- resources/views/header/base-views.blade.php
+++ resources/views/header/base-views.blade.php
@@ -8,6 +8,12 @@
     <!-- Cargar estilos globales -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+    <!-- Cambiar fuente -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+    @stack('styles')
+    <!-- {{-- <link rel="stylesheet" href="{{ asset('cssbootstrap/bootstrap.css') }}"> --}} -->
 </head>
 
 <body>
