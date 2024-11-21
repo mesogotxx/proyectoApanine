bcb564b3799bd78a1558ddbb69685056db8d9bad
Sebastian Loayza <sebastianloayza1309@gmail.com>
2024-11-20 21:06:57 -0300

    a

--- resources/views/header/base-views.blade.php
+++ resources/views/header/base-views.blade.php
@@ -8,17 +8,12 @@
     <!-- Cargar estilos globales -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
-<<<<<<< HEAD
-    @stack('styles')
-    
-=======
     <!-- Cambiar fuente -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
     @stack('styles')
     <!-- {{-- <link rel="stylesheet" href="{{ asset('cssbootstrap/bootstrap.css') }}"> --}} -->
->>>>>>> 5d0d12489986e28a1c8486d01f77d44b2cc60e54
 </head>
 
 <body>
